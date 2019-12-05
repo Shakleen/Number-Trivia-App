7e42288dd677f0b54bbd2e62bab4d05e40e5ad23
Shakleen <shakleenishfar@gmail.com>
2019-12-05 22:03:43 +0600

    Updated get number trivia usecase testcode

--- test/features/number_trivia/domain/usecases/get_concrete_number_trivia_test.dart
+++ test/features/number_trivia/domain/usecases/get_concrete_number_trivia_test.dart
@@ -27,7 +27,7 @@ void main() {
         (_) async => Right(tNumberTrivia),
       );
 
-      final result = await usecase(number: tNumber);
+      final result = await usecase(Params(number: tNumber));
       expect(result, Right(tNumberTrivia));
       verify(mockNumberTriviaRepository.getConcreteNumberTrivia(tNumber));
       verifyNoMoreInteractions(mockNumberTriviaRepository);
