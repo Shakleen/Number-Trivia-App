5b7a66729fc624397c34c664f263c12424083794
Shakleen <shakleenishfar@gmail.com>
2019-12-05 21:52:55 +0600

    Refactoring

--- test/features/number_trivia/domain/usecases/get_concrete_number_trivia_test.dart
+++ test/features/number_trivia/domain/usecases/get_concrete_number_trivia_test.dart
@@ -27,7 +27,7 @@ void main() {
         (_) async => Right(tNumberTrivia),
       );
 
-      final result = await usecase.execute(number: tNumber);
+      final result = await usecase(number: tNumber);
       expect(result, Right(tNumberTrivia));
       verify(mockNumberTriviaRepository.getConcreteNumberTrivia(tNumber));
       verifyNoMoreInteractions(mockNumberTriviaRepository);
