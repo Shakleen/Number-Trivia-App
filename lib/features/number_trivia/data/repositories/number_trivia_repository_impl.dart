cab5dd02c708cd446c2fa167b75a1b7c003d8912
Shakleen <shakleenishfar@gmail.com>
2019-12-07 12:08:06 +0600

    swapped out 3rd party library. refactoring.

--- lib/features/number_trivia/data/repositories/number_trivia_repository_impl.dart
+++ lib/features/number_trivia/data/repositories/number_trivia_repository_impl.dart
@@ -2,7 +2,7 @@ import 'package:dartz/dartz.dart';
 import 'package:flutter/material.dart';
 import 'package:number_trivia/core/error/exception.dart';
 import 'package:number_trivia/core/error/failures.dart';
-import 'package:number_trivia/core/platform/network_info.dart';
+import 'package:number_trivia/core/network/network_info.dart';
 import 'package:number_trivia/features/number_trivia/data/data_sources/number_trivia_local_data_source.dart';
 import 'package:number_trivia/features/number_trivia/data/data_sources/number_trivia_remote_data_source.dart';
 import 'package:number_trivia/features/number_trivia/domain/entities/number_trivia.dart';
