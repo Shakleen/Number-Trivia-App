eee48e463c8b27161e16a2c3973d0ee82149be6a
Shakleen <shakleenishfar@gmail.com>
2019-12-19 20:34:15 +0600

    Basic bloc states created

--- lib/features/number_trivia/presentation/bloc/number_trivia_state.dart
+++ lib/features/number_trivia/presentation/bloc/number_trivia_state.dart
@@ -1,10 +1,35 @@
 import 'package:equatable/equatable.dart';
+import 'package:flutter/material.dart';
+import 'package:number_trivia/features/number_trivia/domain/entities/number_trivia.dart';
 
 abstract class NumberTriviaState extends Equatable {
   const NumberTriviaState();
 }
 
-class InitialNumberTriviaState extends NumberTriviaState {
+class Empty extends NumberTriviaState {
   @override
   List<Object> get props => [];
+}
+
+class Loading extends NumberTriviaState {
+  @override
+  List<Object> get props => [];
+}
+
+class Loaded extends NumberTriviaState {
+  final NumberTrivia numberTrivia;
+
+  Loaded({@required this.numberTrivia});
+
+  @override
+  List<Object> get props => [numberTrivia];
+}
+
+class Error extends NumberTriviaState {
+  final String message;
+
+  Error({@required this.message});
+
+  @override
+  List<Object> get props => [message];
 }
